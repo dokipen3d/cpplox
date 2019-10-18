99a5050fc778e7d03327c048df3a5330112f8317
dokipen3d <danielelliott3d@gmail.com>
2019-10-19 00:31:49 +0100

    add some comments

--- Scanner.cpp
+++ Scanner.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 namespace cpplox {
+////////////////////////////////////////////////////////////////////////
 std::vector<Token> scanTokens(const std::string& code) {
     int start = 0;
     int current = 0;
@@ -10,51 +11,74 @@ std::vector<Token> scanTokens(const std::string& code) {
 
     std::vector<Token> tokens;
 
-    auto advance = [&]() -> char {                               
-        current++;
-        return code[current - 1];                   
-    };
-
     auto addTokenLiteral = [&](ETokenType type, std::any literal) {
-        std::string text = code.substr(start, current-start);      
-        tokens.push_back(Token(type, text, literal, line));    
+        std::string text = code.substr(start, current - start);
+        tokens.push_back(Token(type, text, literal, line));
     };
 
-    auto addToken = [&](ETokenType type) {                
-        addTokenLiteral(type, nullptr);                                
-    };                                                      
+    auto addToken = [&](ETokenType type) { addTokenLiteral(type, nullptr); };
 
-
-    auto scanToken = [&](){                     
-        char c = advance();                          
-        switch (c) {                                 
-            case '(': addToken(ETokenType::LEFT_PARENTHESIS); break;     
-            case ')': addToken(ETokenType::RIGHT_PARENTHESIS); break;    
-            case '{': addToken(ETokenType::LEFT_BRACE); break;     
-            case '}': addToken(ETokenType::RIGHT_BRACE); break;    
-            case ',': addToken(ETokenType::COMMA); break;          
-            case '.': addToken(ETokenType::DOT); break;            
-            case '-': addToken(ETokenType::MINUS); break;          
-            case '+': addToken(ETokenType::PLUS); break;           
-            case ';': addToken(ETokenType::SEMICOLON); break;      
-            case '*': addToken(ETokenType::STAR); break; 
-        }                                            
+    // this moves the 'current' character one forward and then returns the
+    // previous one we just moved past
+    auto advance = [&]() -> char {
+        current++;
+        return code[current - 1];
     };
 
+    // our single function that scans individual tokens. This is called in the
+    // while loop below
+    auto scanToken = [&]() {
+        char c = advance();
+        switch (c) {
+        case '(':
+            addToken(ETokenType::LEFT_PARENTHESIS);
+            break;
+        case ')':
+            addToken(ETokenType::RIGHT_PARENTHESIS);
+            break;
+        case '{':
+            addToken(ETokenType::LEFT_BRACE);
+            break;
+        case '}':
+            addToken(ETokenType::RIGHT_BRACE);
+            break;
+        case ',':
+            addToken(ETokenType::COMMA);
+            break;
+        case '.':
+            addToken(ETokenType::DOT);
+            break;
+        case '-':
+            addToken(ETokenType::MINUS);
+            break;
+        case '+':
+            addToken(ETokenType::PLUS);
+            break;
+        case ';':
+            addToken(ETokenType::SEMICOLON);
+            break;
+        case '*':
+            addToken(ETokenType::STAR);
+            break;
+        }
+    };
+
+    // helper for detecting if we have gone past the end of the file
     auto isAtEnd = [&]() { return current >= code.length(); };
 
     while (!isAtEnd()) {
-        // We are at the beginning of the next lexeme. reset start to be where
-        // we last finished
-        start = current;
-        scanToken();
+
+        start = current; // We are at the beginning of the next lexeme. reset
+                         // start to be where we last finished
+        scanToken(); // scan one token (which adds to the token vector) and then
+                     // the loop continues until it meets the end condition
     }
 
     tokens.push_back(Token(ETokenType::END_OF_FILE, "", nullptr, line));
 
     return tokens;
 }
-
+////////////////////////////////////////////////////////////////////////
 void report(int line, const std::string& where, const std::string& message) {
     std::cout << "[line " << line << "] Error" << where << ": " << message
               << "\n";
@@ -64,4 +88,6 @@ void report(int line, const std::string& where, const std::string& message) {
 void error(int line, const std::string& message) {
     report(line, "", message);
 }
+////////////////////////////////////////////////////////////////////////
+
 } // namespace cpplox
