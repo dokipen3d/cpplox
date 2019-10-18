ca05b355bd5ca6cefc7a34032253c8993e072c35
Daniel Elliott <danielelliott3d@gmail.com>
2019-10-18 15:14:10 +1300

    add basic error handling

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 #include <string>
 #include <vector>
 
+bool hadError = false;
+
 class Scanner {
-public:
-    Scanner(std::string sourceIn) : source(std::move(sourceIn)) {}
-    std::vector<std::string> scanTokens() { return {"token1", "token2"}; }
-private:
+  public:
+    Scanner(std::string sourceIn) : source(std::move(sourceIn)) {
+    }
+    std::vector<std::string> scanTokens() {
+        return {"token1", "token2"};
+    }
+
+  private:
     std::string source;
 };
 
@@ -15,11 +21,22 @@ void run(const std::string& code) {
     Scanner scanner(code);
     auto tokens = scanner.scanTokens();
 
-    //print the tokens
-    for (auto& token : tokens){
-        std::cout << token << "\n"; 
+    // print the tokens
+    for (auto& token : tokens) {
+        std::cout << token << "\n";
     }
 }
+
+void report(int line, const std::string& where, const std::string& message) {
+    std::cout << "[line " << line << "] Error" << where << ": " << message << "\n";
+    hadError = true;
+}
+
+void error(int line, const std::string& message) {
+    report(line, "", message);
+}
+
+
 
 // dummy functions to make main run at this stage
 void runFile(const std::string& filePath) {
@@ -33,6 +50,8 @@ void runPrompt() {
     for (;;) {
         std::getline(std::cin, currentLine);
         run(currentLine);
+        // set error back in case there was an error as we don't want to kill session
+        hadError = false;
     }
 };
 
@@ -43,6 +62,9 @@ int main(int argumentCount, char* argumentValues[]) {
         return 0;
     } else if (argumentCount == 2) {
         runFile(argumentValues[1]);
+        if (hadError) {
+            return 1;
+        }
     } else {
         runPrompt();
     }
