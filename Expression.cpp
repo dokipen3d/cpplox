7bec1270a08a67aaa750dccce9b011292a16ccff
Daniel Elliott <danielelliott3d@gmail.com>
2019-10-21 15:51:18 +1300

    Update Expression.cpp
    need the static cast in the recursive visit

--- Expression.cpp
+++ Expression.cpp
@@ -3,28 +3,22 @@
 #include <iostream>
 #include <vector>
 
-struct Null   { };
 struct Binary;
 struct Literal;
-
 
 template <typename T> struct recursive_wrapper {
   // construct from an existing object
   recursive_wrapper(T t_) { t.emplace_back(std::move(t_)); }
   // cast back to wrapped type
   operator const T &()  { return t.front(); }
-  
-
   // store the value
   std::vector<T> t;
 };
 
-//using Expr = x3::variant<x3::forward_ast<Binary>, x3::forward_ast<Literal>>;
 using Expr = std::variant<recursive_wrapper<Binary>, recursive_wrapper<Literal>>;
 
 struct Binary{
-    //Binary() = default;
-    //using Expr::Expr;
+
     Binary(Expr A, Expr B) : values{A, B}{}
 
     std::vector<Expr> values;
@@ -36,20 +30,23 @@ struct Binary{
 } ;
 
 struct Literal{
-    //using Expr::Expr;
-    //Literal() : Expr(*this){}
     Literal(){};
     int a;
+    Expr asVariant(){
+        return *this;
+    }
 } ;
 
 struct visitor {
     void operator()(const Binary& b) const {
         std::cout << "bin\n";
+        for (auto& e : b.values){
+            std::visit(*this, static_cast<Expr>(e) );
+        }
     }
      void operator()(const Literal& b) const {
         std::cout << "lit\n";
     }
-
 };
 
 int main(){
@@ -60,9 +57,5 @@ int main(){
      auto expr = Binary(Literal{}, Literal{});
 
     std::visit(v, expr.asVariant());
-
 }
-
-
-
 
