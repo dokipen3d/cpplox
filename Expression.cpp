16a8374db0cf77bca3565356967deda0dd20c7e5
Daniel Elliott <danielelliott3d@gmail.com>
2019-10-21 13:58:21 +1300

    Update Expression.cpp
    double virtual

--- Expression.cpp
+++ Expression.cpp
@@ -4,36 +4,45 @@ struct Binary;
 struct Literal;
 
 //template <typename returnType>
-struct exprVisitor {
-    auto visitBeignet( Binary& p){
-        std::cout << "B\n";
-    }
-    auto visitCruller( Literal& p){
-         std::cout << "C\n";
-    }
 
+class Visitor
+{
+  public:
+    virtual void visit(Binary& e) = 0;
+    virtual void visit(Literal& e) = 0;
 };
 
+
 struct Expr {
-    virtual void accept( exprVisitor& visitor){};
+    virtual void accept( Visitor& visitor){};
 };
 
 struct Binary : Expr {
     Binary(Expr A, Expr B) : A(A), B(B){}
-    void accept( exprVisitor& visitor) override {
-        visitor.visitBeignet(*this);
+    void accept( Visitor& visitor) override {
+        visitor.visit(*this);
     }
     Expr A;
     Expr B;
 };
 
 struct Literal : Expr {
-    void accept( exprVisitor& visitor) override {
-        visitor.visitCruller(*this);
+    void accept( Visitor& visitor) override {
+        visitor.visit(*this);
     }
     int a;
 };
 
+struct exprVisitor :  Visitor{
+    void  visit( Binary& p){
+        std::cout << "B\n";
+        p.A.accept(*this);
+    }
+    void visit( Literal& p){
+         std::cout << "C\n";
+    }
+
+};
 
 int main(){
     auto pastry = Binary(Literal{}, Literal{});
