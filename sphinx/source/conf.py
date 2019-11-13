9e7dc23e74d5b2f11f0baacd2c58f1d1b144a92f
Daniel Elliott <danielelliott3d@gmail.com>
2019-11-14 11:00:15 +1300

    recopy files or page

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -40,7 +40,7 @@ release = u'0.0.1'
 # ones.
 extensions = [
     'sphinx.ext.githubpages',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,8 +49,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
